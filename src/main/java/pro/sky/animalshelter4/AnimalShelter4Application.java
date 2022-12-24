46f1d539d8532f8ce09eb4b24ef172c88b872ba0
Zaitcev Viacheslav <slavzick@mail.ru>
2022-12-24 20:51:33 +0500

    Initial commit

---
+++ src/main/java/pro/sky/animalshelter4/AnimalShelter4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.animalshelter4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelter4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelter4Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/animalshelter4/AnimalShelter4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.animalshelter4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelter4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
