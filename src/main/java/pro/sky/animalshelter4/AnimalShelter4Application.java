43493121d1039bb8a5d871bfcedac3a076507c05
slavzick <slavzick@mail.ru>
2023-01-13 04:13:59 +0500

    Add swagger

--- src/main/java/pro/sky/animalshelter4/AnimalShelter4Application.java
+++ src/main/java/pro/sky/animalshelter4/AnimalShelter4Application.java
@@ -1,11 +1,13 @@
 package pro.sky.animalshelter4;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition
 public class AnimalShelter4Application {
 
     public static void main(String[] args) {
